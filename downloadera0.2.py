6c0a8227790d8f7bcb7765c96f8864cd4db89968
joao41fi <97768350+joao41fi@users.noreply.github.com>
2023-01-04 16:04:04 +0000

    Adição de estética e de ficheiros de testes
    como uma img

--- downloadera0.2.py
+++ downloadera0.2.py
@@ -1,7 +1,7 @@
 
 
 from pytube import *
-from pytube.cli import on_progress
+from tkinter.messagebox import showinfo
 from tkinter import * 
 from tkinter.ttk import *
 from tkinter import filedialog 
@@ -9,7 +9,7 @@ import _thread
 import progressbar as progress
 import os    
 
-
+from tkinter import ttk
 global pasta
 u_name = os.getlogin()
 print(f"[*] Username is : {u_name}")
@@ -21,20 +21,16 @@ def progress(streams, chunk: bytes, bytes_remaining: int):
     contentsize = ys.filesize
     size = contentsize - bytes_remaining
 
-    #print('\r' + '[Download progress]:[%s%s]%.2f%%;' % (
+    #print('\r' + '[Download progress]:[%s%s]%.2f%%;' % 
     #'█' * int(size*20/contentsize), ' '*(20-int(size*20/contentsize)), float(size/contentsize*100)), end='')
     #print(size,bytes_remaining,contentsize)   #contentsize == 100
     progresse['value'] = size/contentsize*100
     ferame.update_idletasks()
     return print(size,bytes_remaining,contentsize) 
                                               
-
-
-
 def baixas(link):
     global ys
    
-    
     yt  = YouTube(link, on_progress_callback= progress)
     
     ys = yt.streams.get_highest_resolution()
@@ -52,6 +48,36 @@ def pasta ():
  global pastas 
  pastas = filedialog.askdirectory(initialdir="YOUR DIRECTORY PATH") 
 
+def video(link):
+ resol = []
+ try:
+    
+  
+  yt  = YouTube(link)
+  videos = yt.streams
+ 
+ 
+  for i in range(len(videos)):
+    print(i,videos[i])
+    ok= videos[i]
+    ok = str(ok)
+    ok =ok.split(None)
+    resol.append(ok[3])
+  
+  return(resol)
+ except:
+    return('ok')
+
+def loop():
+    try:
+     link = biaxa.get()
+    except:
+        link = ''
+    #if link != '' and  video(link) != 'ok' :
+    # month_cb['values'] = video(link)
+    else:
+     loop()
+ 
 janela = janela = Tk()
 win_width, win_height = 200, 200
 janela.geometry(f'{win_width}x{win_height}')
@@ -76,6 +102,24 @@ bot.place(x=110,y=80)
 
 bot= Button(janela,text="Baixa",command = lambda:ok()) 
 bot.place(x=30,y=80)
+
+label = ttk.Label( janela ,text="Please select a month:")
+label.place(x=50,y=135)
+
+
+month_cb = ttk.Combobox(janela)
+
+
+
+
+
+try:
+ month_cb.current(5) 
+except :
+  print
+  
+month_cb.place(x=50,y=155)
+loop()
 janela.mainloop()
 
   
